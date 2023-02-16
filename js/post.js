6245ee3a602ea9bb3c6b70f81f690c90ab2a51a8
BENEDEK László <lacbenedek@gmail.com>
2023-02-16 19:01:07 +0100

    Added image upload form elements and basic JS.

--- js/post.js
+++ js/post.js
@@ -4,6 +4,7 @@ function removePost(id) {
     }
     window.location = window.location;
 }
+
 function removeReply(id) {
     if(confirm("Are you sure you want to remove this relpy?")) {
         console.log("TODO: remove reply");
@@ -13,5 +14,26 @@ function removeReply(id) {
 
 function likePost(id) {
     console.log("TODO: like post");
+    window.location = window.location;
+}
+
+var fileEncoded = null;
+function encodeFile() {
+    let files = document.getElementById("file").files;
+    if (files.length != 1) return;
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onloadend = () => {
+        fileEncoded = reader.result;
+    }
+}
+
+function makePost() {
+    console.log("TODO: make post");
+    window.location = window.location;
+}
+
+function makeReply() {
+    console.log("TODO: make reply");
     window.location = window.location;
 }
