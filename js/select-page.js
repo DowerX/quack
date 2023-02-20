9265e3ba12072c080c74c11ddce47e8103a930a4
BENEDEK László <lacbenedek@gmail.com>
2023-02-20 01:25:16 +0100

    Fixed the page selector on search.php.

--- js/select-page.js
+++ js/select-page.js
@@ -1,31 +1,28 @@
 var limit = 10;
 var offset = 0;
+var extra = "";
 
 function parseParams() {
     let params = new URLSearchParams(window.location.search);
     if(params.has("offset")) offset = Number.parseInt(params.get("offset"));
     if(params.has("limit")) limit = Number.parseInt(params.get("limit"));
-}
-
-function id() {
-    let params = new URLSearchParams(window.location.search);
-    if(params.has("id")) return `&id=${params.get("id")}`;
-    return "";
+    if(params.has("query")) extra += `&query=${params.get("query")}`;
+    if(params.has("id")) extra += `&id=${params.get("id")}`;
 }
 
 function start() {
     parseParams();
-    window.location = `${window.location.pathname}?limit=${limit}&offset=0${id()}`;
+    window.location = `${window.location.pathname}?limit=${limit}&offset=0${extra}`;
 }
 function end() {
     parseParams();
-    window.location = `${window.location.pathname}?limit=${limit}&offset=0${id()}`;
+    window.location = `${window.location.pathname}?limit=${limit}&offset=0${extra}`;
 }
 function prev() {
     parseParams();
-    window.location = `${window.location.pathname}?limit=${limit}&offset=${Math.max(offset-limit, 0)}${id()}`;
+    window.location = `${window.location.pathname}?limit=${limit}&offset=${Math.max(offset-limit, 0)}${extra}`;
 }
 function next() {
     parseParams();
-    window.location = `${window.location.pathname}?limit=${limit}&offset=${offset+limit}${id()}`;
+    window.location = `${window.location.pathname}?limit=${limit}&offset=${offset+limit}${extra}`;
 }
