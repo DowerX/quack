587eceafe6153f404d328d9283d71e8d7250ac68
BENEDEK László <lacbenedek@gmail.com>
2023-02-23 11:11:29 +0100

    Replace URLs in posts with links.

--- js/mention.js
+++ js/mention.js
@@ -1,8 +1,27 @@
 function replaceMentions(text) {
-    const user = /@([\wöüóőúéáűÖÜŐÚÉÁŰ]+)/;
-    const post = /#([0-9]+)/;
-    text = text.replace(user, '<a href="/php/api/redirect-username.php?username=$1" class="mention">@$1</a>');
-    return text.replace(post, '<a href="/php/post.php?id=$1" class="mention">#$1</a>');
+    const patterns = [
+        // @user
+        { regex: /@([\wöüóőúéáűÖÜŐÚÉÁŰ]+)/,
+          replace: '<a href="/php/api/redirect-username.php?username=$1" class="mention">@$1</a>'
+        },
+        // #postid
+        { regex: /#([0-9]+)/,
+          replace: '<a href="/php/post.php?id=$1" class="mention">#$1</a>'
+        },
+        // URL
+        { regex: /(https|http):\/\/([\wöüóőúéáűÖÜÓŐÚÉÁŰ\.]+)([\wöüóőúéáűÖÜÓŐÚÉÁŰ\/\.\?\=\#\&\%\-\;]+)?/,
+          replace: (match, g1, g2, g3) => {
+            if (g3) {
+                return `<a href="${match}" class="mention">${g2}/...${g3.slice(-Math.min(5, g3.length-1))}</a>`;
+            } else {
+                return `<a href="${match}" class="mention">${g2}</a>`;
+            }
+        }},
+    ];
+    patterns.forEach((p) => {
+        text = text.replace(p.regex, p.replace);
+    });
+    return text;
 }
 
 document.querySelectorAll(".posts p").forEach((e) => {
