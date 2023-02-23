6c5a893686f61b301f3fd0d536053d3151c0c038
BENEDEK László <lacbenedek@gmail.com>
2023-02-23 11:40:45 +0100

    Replace new lines with <br> in posts.

--- js/mention.js
+++ js/mention.js
@@ -1,19 +1,23 @@
 function replaceMentions(text) {
     const patterns = [
         // @user
-        { regex: /@([\wöüóőúéáűÖÜŐÚÉÁŰ]+)/,
+        { regex: /@([\wöüóőúéáűÖÜŐÚÉÁŰ]+)/g,
           replace: '<a href="/php/api/redirect-username.php?username=$1" class="mention">@$1</a>'
         },
         // #postid
-        { regex: /#([0-9]+)/,
+        { regex: /#([0-9]+)/g,
           replace: '<a href="/php/post.php?id=$1" class="mention">#$1</a>'
         },
         // spoiler:"some spoiler"
-        { regex: /spoiler:"(.+)"/,
+        { regex: /spoiler:"(.+)"/g,
           replace: '<mark class="spoiler">$1</mark>'
         },
+        // new line
+        { regex: /\n/g,
+          replace: '<br>'
+        },
         // URL
-        { regex: /(https|http):\/\/([\wöüóőúéáűÖÜÓŐÚÉÁŰ\.]+)([\wöüóőúéáűÖÜÓŐÚÉÁŰ\/\.\?\=\#\&\%\-\;]+)?/,
+        { regex: /(https|http):\/\/([\wöüóőúéáűÖÜÓŐÚÉÁŰ\.]+)([\wöüóőúéáűÖÜÓŐÚÉÁŰ\/\.\?\=\#\&\%\-\;]+)?/g,
           replace: (match, g1, g2, g3) => {
             if (g3) {
                 return `<a href="${match}" class="mention">${g2}/...${g3.slice(-Math.min(5, g3.length-1))}</a>`;
