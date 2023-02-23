505bd77dfb65031048a8268027b5bbb2a42b63e7
BENEDEK László <lacbenedek@gmail.com>
2023-02-23 17:18:18 +0100

    Changed login bg style.

--- js/mouse-pointer.js
+++ js/mouse-pointer.js
@@ -1,10 +1,15 @@
 function initMousePointer() {
-    var pointer = document.createElement("div");
+    const pointer = document.createElement("div");
     pointer.id = "pointer";
     document.body.appendChild(pointer);
 
+    const main = document.getElementsByTagName("main")[0];
+    const login = document.getElementById("login-box");
+
     document.onmousemove = (e) => {
+        var mainRect = main.getBoundingClientRect();
         pointer.style =  `--pos-x: ${e.clientX}px; --pos-y: ${e.clientY}px`;
+        main.style =  `--pos-x: ${e.clientX}px; --pos-y: ${e.clientY}px`;
     }
 }
 
